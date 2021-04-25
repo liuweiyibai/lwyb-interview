e895f52f852c9b287fadd10697c186c94aaa9886
liuwei <liuwei@yunheit.com>
2021-04-25 17:13:29 +0800

    更新logo

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,12 +1,20 @@
 module.exports = {
   title: '面试题集合',
+  lang: 'zh-cn',
   description: '收集前后端常见面试题，以及自己学习 vue、react 源码过程的笔记',
+  base: '/interview/',
+  head: [
+    [
+      'link', // 设置 favicon.ico，注意图片放在 public 文件夹下
+      { rel: 'icon', href: 'logo.webp' },
+    ],
+  ],
   themeConfig: {
-    repo: 'vuejs/vitepress',
+    repo: 'liuweiyibai/lwyb-interview',
     docsDir: 'docs',
     editLinks: true,
     editLinkText: 'Edit this page on GitHub',
-    lastUpdated: 'Last Updated',
+    lastUpdated: '上次更新时间',
     // nav: [
     //   { text: 'Guide', link: '/', activeMatch: '^/$|^/guide/' },
     //   {
@@ -33,13 +41,10 @@ function getGuideSidebar() {
     {
       text: 'js面试题',
       children: [
-        { text: '手写js 中 bind', link: '/js/bind' },
-        { text: '手写js 中 call 和 apply', link: '/js/call_apply' },
-        { text: 'Configuration', link: '/guide/configuration' },
-        { text: 'Asset Handling', link: '/guide/assets' },
-        { text: 'Markdown Extensions', link: '/guide/markdown' },
-        { text: 'Using Vue in Markdown', link: '/guide/using-vue' },
-        { text: 'Deploying', link: '/guide/deploy' },
+        { text: '手写 js 中 bind', link: '/js/bind' },
+        { text: '手写 js 中 call 和 apply', link: '/js/call_apply' },
+        { text: '手写 js 中 Promise api', link: '/js/promise' },
+        { text: '手写 js 中数组 forEach', link: '/js/array/foreach' },
       ],
     },
     {
