6e32c0bdf8ddd18dd5ad34d021e78cb2cedb77ca
hotblin <lw1140@163.com>
2021-05-03 17:02:16 +0800

    update

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -44,8 +44,8 @@ function getGuideSidebar() {
         { text: '常见js面试题', link: '/js/' },
         { text: '手写 js 中 bind', link: '/js/bind' },
         { text: '手写 js 中 call 和 apply', link: '/js/call_apply' },
-        { text: '手写 js 中 Promise api', link: '/js/promise' },
-        { text: '手写 js 中数组 forEach', link: '/js/array/foreach' },
+        // { text: '手写 js 中 Promise api', link: '/js/promise' },
+        // { text: '手写 js 中数组 forEach', link: '/js/array/foreach' },
         { text: 'js 中的类型和类型判断', link: '/js/typeof' },
         { text: '手写 js 对象中的 create 函数', link: '/js/object/create' },
         { text: '定时器相关', link: '/js/setTimeout' },
@@ -68,13 +68,25 @@ function getGuideSidebar() {
       children: [
         {
           text: '排序',
-          link: '/算法/sort',
+          link: '/algorithm/sort',
+        },
+      ],
+    },
+    {
+      text: 'HTTP 面试题',
+      children: [
+        {
+          text: '常见 HTTP 状态码',
+          link: '/http/statusCode',
+        },
+        {
+          text: 'axios 相关',
+          link: '/http/axios',
         },
       ],
     },
     {
       text: 'css 面试题',
-      children: [{ text: 'Frontmatter', link: '/guide/frontmatter' }],
     },
     {
       text: 'html 面试题',
